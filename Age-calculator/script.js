4960a9479215db5ea2ff2d0271623976a601370a
dragonilly <134251470+JikokuAice@users.noreply.github.com>
2023-08-23 17:43:30 +0545

    age calculator
    (commit-1) just starting with basic stuff

---
+++ Age-calculator/script.js
@@ -0,0 +1,7 @@
+function linktogit(){
+window.location='https://github.com/JikokuAice/ProjectOpus';
+}
+
+function linktolinkidin(){
+    window.location='https://www.linkedin.com/in/ayush-karki-2541b0282/';
+}
